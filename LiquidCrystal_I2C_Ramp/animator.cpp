76f2c62ff1338986a08ccdb6871e9efed3ce01ba
mikand <mikkelsen.anders@gmail.com>
2015-10-08 09:12:37 +0200

    initial commit

---
+++ LiquidCrystal_I2C_Ramp/animator.cpp
@@ -0,0 +1,64 @@
+#ifndef ANIMATOR_CPP
+#define ANIMATOR_CPP
+
+#include "animator.h"
+
+Animator::Animator(byte address, int rampLow, int rampHigh) 
+    : rampLow(rampLow),rampHigh(rampHigh) {
+  *lcd = LiquidCrystal_I2C(address, COLUMNS, ROWS);
+}
+
+void Animator::lcdRamp (int value) {  
+  lcd->clear();  
+
+  int lcdValue = map(value, rampLow, rampHigh, 0, COLUMNS);
+
+  if (lcdValue > COLUMNS) {
+    lcdValue = COLUMNS;
+  } else if (lcdValue < 0) {
+    lcdValue = 0;
+  }
+
+  for (int i = 0; i < lcdValue; i++){
+    lcd->setCursor(i, 1);
+    
+    if (i == 0) {
+      lcd->write(i);
+    } else if (i > COLUMNS / 2) {
+      lcd->write(COLUMNS / 2 - 1);
+
+      lcd->setCursor(i, 0);
+      lcd->write(i - (COLUMNS / 2 + 1));
+    } else {
+      lcd->write(i - 1);
+    }
+  }
+} 
+
+void Animator::makeLcdChars() {
+  byte ramp[COLUMNS / 2][8] = { 
+      { 0b00000, 0b00000, 0b00000, 0b00000, 0b00000, 0b00000, 0b00000, 0b11111 }, 
+      { 0b00000, 0b00000, 0b00000, 0b00000, 0b00000, 0b00000, 0b11111, 0b11111 }, 
+      { 0b00000, 0b00000, 0b00000, 0b00000, 0b00000, 0b11111, 0b11111, 0b11111 }, 
+      { 0b00000, 0b00000, 0b00000, 0b00000, 0b11111, 0b11111, 0b11111, 0b11111 }, 
+      { 0b00000, 0b00000, 0b00000, 0b11111, 0b11111, 0b11111, 0b11111, 0b11111 }, 
+      { 0b00000, 0b00000, 0b11111, 0b11111, 0b11111, 0b11111, 0b11111, 0b11111 }, 
+      { 0b00000, 0b11111, 0b11111, 0b11111, 0b11111, 0b11111, 0b11111, 0b11111 }, 
+      { 0b11111, 0b11111, 0b11111, 0b11111, 0b11111, 0b11111, 0b11111, 0b11111 } 
+    };
+    
+  for (int i = 0; i < COLUMNS / 2; i++) {
+    lcd->createChar(i, ramp[i]);
+  }
+}
+
+void Animator::setUpLcd() {
+  lcd->begin(COLUMNS, ROWS);
+  lcd->init();
+  lcd->backlight();
+  
+  // set up ramp
+  makeLcdChars(); 
+}
+
+#endif
---
+++ LiquidCrystal_I2C_Ramp/animator.h
@@ -0,0 +1,34 @@
+#ifndef ANIMATOR_H
+#define ANIMATOR_H
+
+#define COLUMNS 16
+#define ROWS 2
+
+#include <Arduino.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+/**
+ * This class defines the ramp animator for music. It initializes the LiquidCrystal_I2C lib
+ * and accepts commands for ramping from 0 to 16. We default any values outside 0 - 16 to
+ * 0 or 16 respectively.
+ * 
+ * @author Anders Mikkelsen
+ * @version 04.10.2015
+ */
+class Animator {
+  public:
+    Animator(byte address, int rampLow, int rampHigh);
+  
+    void setUpLcd();
+    void lcdRamp (int value);
+  private:
+    int rampLow;
+    int rampHigh;
+  
+    void makeLcdChars();
+
+    LiquidCrystal_I2C* lcd;
+};
+
+#endif
